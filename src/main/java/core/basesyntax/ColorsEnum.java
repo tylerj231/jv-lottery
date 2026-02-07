ce74ea18be7b63ef59f32536cf47b5c6d84e8845
Jack Tyler <hepavista@gmail.com>
2026-02-07 19:04:01 +0200

    Solution implementation

---
+++ src/main/java/core/basesyntax/ColorsEnum.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public enum ColorsEnum {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    VIOLET,
+}
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    public static Ball getRandomBall() {
+        String randomColor = ColorSupplier.getRandomColor();
+        int randomNumber = new Random().nextInt(100);
+        return new Ball(randomColor, randomNumber);
+    }
+}
